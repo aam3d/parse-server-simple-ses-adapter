08bf903ba05b63db2e67c3fb7612122de4779bfa
leevers <leevers@gmail.com>
2020-09-03 15:03:54 +1000

    Bump version Prettier pretties

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-var ses = require('node-ses');
+var ses = require("node-ses");
 const hbs = require("handlebars");
 const fs = require("fs");
 const path = require("path");
 
-var SimpleSESAdapter = sesOptions => {
-
-  if (!sesOptions || !sesOptions.apiKey || !sesOptions.apiSecret || !sesOptions.domain || !sesOptions.fromAddress) {
-    throw 'SimpleSESAdapter requires an API Key, domain, and fromAddress.';
-  }
-  
-  if(!sesOptions.amazon){
-    sesOptions.amazon = 'https://email.us-east-1.amazonaws.com';
-  }
-
-  if(!sesOptions.verificationTemplate){
-    sesOptions.verificationTemplate = './templates/verificationTemplate.html';
+var SimpleSESAdapter = (sesOptions) => {
+  if (
+    !sesOptions ||
+    !sesOptions.apiKey ||
+    !sesOptions.apiSecret ||
+    !sesOptions.domain ||
+    !sesOptions.fromAddress
+  ) {
+    throw "SimpleSESAdapter requires an API Key, domain, and fromAddress.";
   }
 
-  if(!sesOptions.amazon){
-    sesOptions.passwordResetTemplate = './templates/passwordResetTemplate.html';
+  if (!sesOptions.amazon) {
+    sesOptions.amazon = "https://email.us-east-1.amazonaws.com";
   }
-  
-  var client = ses.createClient({ key: sesOptions.apiKey, secret: sesOptions.apiSecret, amazon : sesOptions.amazon });
 
-  var sendMail = mail => {
+  if (!sesOptions.verificationTemplate) {
+    sesOptions.verificationTemplate = "./templates/verificationTemplate.html";
+  }
+
+  if (!sesOptions.amazon) {
+    sesOptions.passwordResetTemplate = "./templates/passwordResetTemplate.html";
+  }
+
+  var client = ses.createClient({
+    key: sesOptions.apiKey,
+    secret: sesOptions.apiSecret,
+    amazon: sesOptions.amazon,
+  });
+
+  var sendMail = (mail) => {
     var data = {
       to: mail.to,
       from: sesOptions.fromAddress,
       subject: mail.subject,
     };
 
-    if (sesOptions.format === 'text') {
+    if (sesOptions.format === "text") {
       data.altText = mail.text;
     } else {
       data.message = mail.text;
     }
 
     return new Promise((resolve, reject) => {
-      client.sendEmail(data, function(err, body, res) {
-        if (typeof err !== 'undefined' && err) {
+      client.sendEmail(data, function (err, body, res) {
+        if (typeof err !== "undefined" && err) {
           console.log(err);
           reject(err);
         }
@@ -46,30 +55,37 @@ var SimpleSESAdapter = sesOptions => {
       });
     });
   };
-  
-  const sendVerificationEmail = data => {
+
+  const sendVerificationEmail = (data) => {
     console.log(data);
     const { user, appName } = data;
     return new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, sesOptions.verificationTemplate), "utf-8", (error, buffer) => {
-        if (error) {
-          reject(error);
-        } else {
-          var mailData = {
-            text: hbs.compile(buffer),
-            to: user.get("email") || user.get("username"),
-            subject: "Please verify your E-mail with " + appName,
-          };
-          resolve(sendMail(mailData));
+      fs.readFile(
+        path.join(__dirname, sesOptions.verificationTemplate),
+        "utf-8",
+        (error, buffer) => {
+          if (error) {
+            reject(error);
+          } else {
+            var mailData = {
+              text: hbs.compile(buffer),
+              to: user.get("email") || user.get("username"),
+              subject: "Please verify your E-mail with " + appName,
+            };
+            resolve(sendMail(mailData));
+          }
         }
-      });
+      );
     });
-  }
-  
-  const sendPasswordResetEmail = data => {
-      const { user, appName } = data;
-      return new Promise((resolve, reject) => {
-        fs.readFile(path.join(__dirname,sesOptions.passwordResetTemplate), "utf-8", (error, buffer) => {
+  };
+
+  const sendPasswordResetEmail = (data) => {
+    const { user, appName } = data;
+    return new Promise((resolve, reject) => {
+      fs.readFile(
+        path.join(__dirname, sesOptions.passwordResetTemplate),
+        "utf-8",
+        (error, buffer) => {
           if (error) {
             reject(error);
           } else {
@@ -80,14 +96,15 @@ var SimpleSESAdapter = sesOptions => {
             };
             resolve(sendMail(mailData));
           }
-        });
-      });
-  }
+        }
+      );
+    });
+  };
 
   return Object.freeze({
     sendMail: sendMail,
     sendVerificationEmail,
-    sendPasswordResetEmail
+    sendPasswordResetEmail,
   });
 };
 
